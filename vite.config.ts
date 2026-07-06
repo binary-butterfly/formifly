2f31a7ba6cf4316986f596dc0d731a2c3aed9d6c
Jennifer Kirsch <jennifer.kirsch@binary-butterfly.de>
2026-07-06 12:51:12 +0200

    Remove rolldown runtime helper from bundle

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ import react, {reactCompilerPreset} from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import babel from '@rolldown/plugin-babel';
 
+// I know how this looks. If you find a better way to not have the bundle throw errors about imaginary require calls, be my guest.
+const stripRolldownRuntimePlugin = () => ({
+    name: 'strip-rolldown-runtime',
+    renderChunk(code: string) {
+        if (code.includes('rolldown/runtime.js')) {
+            return {
+                code: code.replace(/\/\/#region\s\\0rolldown\/runtime\.js(.|\n)*?\/\/#endregion/gm, ''),
+                map: null, // Disables complex remap shifts
+            };
+        }
+        return null;
+    },
+});
+
 // https://vitejs.dev/config/
 export default ({mode}: ConfigEnv): UserConfigExport => {
     process.env = {...process.env, ...loadEnv(mode, process.cwd())};
@@ -22,7 +36,9 @@ export default ({mode}: ConfigEnv): UserConfigExport => {
             ),
             babel({
                 presets: [reactCompilerPreset()],
+                exclude: [/node_modules/],
             }),
+            stripRolldownRuntimePlugin(),
         ],
         build: {
             sourcemap: true,
@@ -36,7 +52,7 @@ export default ({mode}: ConfigEnv): UserConfigExport => {
                 fileName: (format, entryName) => `${entryName}.${format}.js`,
                 formats: ['es'],
             },
-            rollupOptions: {
+            rolldownOptions: {
                 external: [
                     'react',
                     'react-dom',
